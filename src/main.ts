ac24d6e7a9e307baab43018d6190304d0844be71
secsec2025 <116304364+secsec2025@users.noreply.github.com>
2023-10-30 01:03:08 +0530

    Added processor config

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
-import {TypeormDatabase} from '@subsquid/typeorm-store'
-import {processor} from './processor'
+import {TypeormDatabase} from '@subsquid/typeorm-store';
+import {processor} from './processor';
 
 processor.run(new TypeormDatabase({supportHotBlocks: true}), async (ctx) => {
-    throw new Error(`Batch handler is not implemented`)
+    console.log(`Batch Size - ${ctx.blocks.length} blocks`);
+
+    for (const c of ctx.blocks) {
+        for (const e of c.logs) {
+            if (e.address === '0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB'.toLowerCase()) {
+                // console.log('from punks');
+            } else if (e.address === '0xb7F7F6C52F2e2fdb1963Eab30438024864c313F6'.toLowerCase()) {
+                // console.log('from wrapped punks');
+            } else {
+                // console.log('some other');
+            }
+        }
+    }
 })
--- src/processor.ts
+++ src/processor.ts
@@ -1,15 +1,61 @@
-import {EvmBatchProcessor} from '@subsquid/evm-processor'
-import {lookupArchive} from '@subsquid/archive-registry'
+import {EvmBatchProcessor} from '@subsquid/evm-processor';
+import {lookupArchive} from '@subsquid/archive-registry';
+
+import {events as cryptoPunksEvents } from './abi/cryptopunks';
+import {events as wrappedPunksEvents } from './abi/wrappedpunks';
+import {events as erc721SaleEvents } from './abi/ERC721Sale';
+import {events as raribleV1Events } from './abi/RaribleExchangeV1';
+import {events as openSeaEvents } from './abi/Opensea';
+
+import {CRYPTOPUNKS_CONTRACT_ADDRESS, WRAPPEDPUNKS_CONTRACT_ADDRESS, ERC721SALE_CONTRACT_ADDRESS,
+    RARIBLE_V1_CONTRACT_ADDRESS, OPENSEA_CONTRACT_ADDRESS} from './constants';
 
 export const processor = new EvmBatchProcessor()
     .setDataSource({
         archive: lookupArchive('eth-mainnet'),
-        chain: 'https://my.eth-mainnet.rpc',
+        chain: 'https://rpc.ankr.com/eth',
     })
-    .setFinalityConfirmation(10)
-    .setFields({
-        // specify field selection here
+    .setFinalityConfirmation(75)
+    .addLog({
+        address: [CRYPTOPUNKS_CONTRACT_ADDRESS],
+        topic0: [
+            cryptoPunksEvents.Assign.topic,
+            cryptoPunksEvents.PunkTransfer.topic,
+            cryptoPunksEvents.Transfer.topic,
+            cryptoPunksEvents.PunkOffered.topic,
+            cryptoPunksEvents.PunkBidEntered.topic,
+            cryptoPunksEvents.PunkBidWithdrawn.topic,
+            cryptoPunksEvents.PunkBought.topic,
+            cryptoPunksEvents.PunkNoLongerForSale.topic
+        ],
+        range: { from: 3914494 }
     })
     .addLog({
-        address: [/* set of requested addresses */]
+        address: [WRAPPEDPUNKS_CONTRACT_ADDRESS],
+        topic0: [
+            wrappedPunksEvents.Transfer.topic,
+            wrappedPunksEvents.ProxyRegistered.topic
+        ],
+        range: { from: 10821736 }
     })
+    .addLog({
+        address: [ERC721SALE_CONTRACT_ADDRESS],
+        topic0: [
+            erc721SaleEvents.Buy.topic
+        ],
+        range: { from: 10786971 }
+    })
+    .addLog({
+        address: [RARIBLE_V1_CONTRACT_ADDRESS],
+        topic0: [
+            raribleV1Events.Buy.topic
+        ],
+        range: { from: 11274515 }
+    })
+    .addLog({
+        address: [OPENSEA_CONTRACT_ADDRESS],
+        topic0: [
+            openSeaEvents.OrdersMatched.topic
+        ],
+        range: { from: 5774644 }
+    });
